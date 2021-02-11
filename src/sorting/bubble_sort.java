10869197994293cac2ffb164eeb053011095b7e4
HP <HP@LAPTOP-2TUUS30E>
2021-02-11 21:41:59 +0530

    merge_branch

--- src/sorting/bubble_sort.java
+++ src/sorting/bubble_sort.java
@@ -8,7 +8,6 @@ public class bubble_sort {
 	         for(int i=0; i < n; i++){  
 	                 for(int j=1; j < (n-i); j++){  
 	                          if(arr[j-1] > arr[j]){  
-	                                 //swap elements  
 	                                 temp = arr[j-1];  
 	                                 arr[j-1] = arr[j];  
 	                                 arr[j] = temp;  
--- src/sorting/merge_sort.java
+++ src/sorting/merge_sort.java
@@ -13,7 +13,7 @@ public class merge_sort {
         for (int i=0; i<n1; ++i) 
             L[i] = arr[l + i]; 
         for (int j=0; j<n2; ++j) 
-            R[j] = arr[m + 1+ j]; 
+            R[j] = arr[m + 1+ j];   
         int i = 0, j = 0; 
         int k = l; 
         while (i < n1 && j < n2) 
